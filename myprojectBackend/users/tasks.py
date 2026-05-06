e641af22ff9171e8f7354ed1739045d46c157912
rohanthechamp <malverohan90@gmail.com>
2026-05-06 16:33:43 +0530

    imporved celery tasks

--- myprojectBackend/users/tasks.py
+++ myprojectBackend/users/tasks.py
@@ -1,15 +1,10 @@
 from celery import shared_task
-
 import logging
-from celery import group
-from django.conf import settings
-from smtplib import SMTPConnectError
-from django.core.mail import EmailMultiAlternatives
-from django.template.loader import render_to_string
-from django.utils.html import strip_tags
 
 from users.utils import send_invite_email
+
 logger = logging.getLogger("django")
+
 
 @shared_task(
     bind=True,
@@ -18,5 +13,38 @@ logger = logging.getLogger("django")
     retry_backoff_max=60,
     retry_kwargs={"max_retries": 5},
 )
-def send_invite_email_task(email, invite_link, hotel_name,resend=None):
-    send_invite_email(email, invite_link, hotel_name,resend)
+def send_invite_email_task(
+    self,
+    email,
+    invite_link,
+    hotel_name,
+    resend=None,
+):
+    task_id = self.request.id
+    retry_count = self.request.retries
+
+    logger.info(
+        f"📨 [TASK STARTED] task_id={task_id} | "
+        f"email={email} | hotel={hotel_name} | retries={retry_count}"
+    )
+
+    try:
+        send_invite_email(
+            email,
+            invite_link,
+            hotel_name,
+            resend,
+        )
+
+        logger.info(
+            f"✅ [EMAIL SENT SUCCESSFULLY] " f"task_id={task_id} | recipient={email}"
+        )
+
+    except Exception as e:
+        logger.error(
+            f"❌ [EMAIL TASK FAILED] "
+            f"task_id={task_id} | recipient={email} | error={str(e)}",
+            exc_info=True,
+        )
+
+        raise
