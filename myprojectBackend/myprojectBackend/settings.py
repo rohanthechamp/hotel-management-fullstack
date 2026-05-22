03d25d6d4cf285164fcf6b09e3b1238367bb11d0
rohanthechamp <malverohan90@gmail.com>
2026-05-22 22:47:28 +0530

    `Removed trailing slash from CORS_ALLOWED_ORIGINS`

--- myprojectBackend/myprojectBackend/settings.py
+++ myprojectBackend/myprojectBackend/settings.py
@@ -98,7 +98,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:3001",
     "https://hotel-management-fullstack-ten.vercel.app",
-    "https://stayease-app-zeta.vercel.app/",
+    "https://stayease-app-zeta.vercel.app",
     "https://hote-customer-facing-website-1h9r43uf8-rohanthechamps-projects.vercel.app",
 ]
 
